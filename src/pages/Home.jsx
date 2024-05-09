851c3f3adb8264c321d9acf46efb763a703126d8
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-05-09 21:04:40 +0200

    9 - Working With The GitHub API -> 1 - GitHub API & Token

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ function Home() {
   return (
     <div>
       <h1 className="text-6xl">Welcome</h1>
+      {import.meta.env.VITE_APP_GITHUB_TOKEN}   {/* Just to see if it is possible to output data from the .env file. */}
     </div>
   )
 }
