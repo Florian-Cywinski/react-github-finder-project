37fd824a9fb8946378dc2b3efe997872e87756ef
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-05-10 12:56:14 +0200

    9 - Working With The GitHub API -> 5 - Setup GitHub Context

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,26 @@ import Footer from './components/layout/Footer'
 import Home from './pages/Home'
 import About from './pages/About'         // <About />
 import NotFound from './pages/NotFound'
+import { GithubProvider } from './context/github/GithubContext'
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />       {/* if the user is on localhost:3000/about it will be shown everything what is within <About /> */}
-            <Route path='/notfound' element={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />        {/* /* for the case the user types in a url that doesn't exist */}
-          </Routes>
-        </main>
-        <Footer/>
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />       {/* if the user is on localhost:3000/about it will be shown everything what is within <About /> */}
+              <Route path='/notfound' element={<NotFound />} />
+              <Route path='/*' element={<NotFound />} />        {/* /* for the case the user types in a url that doesn't exist */}
+            </Routes>
+          </main>
+          <Footer/>
+        </div>
+      </Router>
+    </GithubProvider>
   )
 }
 
