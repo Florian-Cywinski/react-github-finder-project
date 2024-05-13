f0c65aeaee2ce676cbbe39c567fba3a555b2af31
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-05-13 22:39:21 +0200

    10 - User Profile & Alerts -> 7 - Repo Items

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,20 +1,15 @@
 import PropTypes from 'prop-types'
-// import RepoItem from './RepoItem'
+import RepoItem from './RepoItem'
 
 function RepoList({ repos }) {  // repos is an array of all repos from the wanted user
   return (
     <>
       <div className='rounded-lg shadow-lg card bg-base-100'>
         <div className='card-body'>
-          {/* <h2 className='text-3xl my-4 font-bold card-title'>Latest Repositories</h2> */}
-          {/* {repos} */}
-          {/* {repos.map((repo) => (
-            <h3>{repo.name}</h3>
-            // <RepoItem key={repo.id} repo={repo} />
-          ))} */}
           <h2 className='text-3xl my-4 font-bold card-title'>Latest Repositories</h2>
             {repos.map((repo) => (
-              <h3 key={repo.id}>{repo.name}</h3>
+              // <h3 key={repo.id}>{repo.name}</h3>
+            <RepoItem key={repo.id} repo={repo} />
           ))}
         </div>
       </div>
