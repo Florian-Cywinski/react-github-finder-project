d76c613e6adda1972019c5d314d16dd42525d95d
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-05-14 21:52:48 +0200

    11 - Refactoring Context & Actions -> 3 - Cleaning Up Our Actions & Axios -> Cleaned up: Unnecessary comments removed

--- src/context/github/GithubActions.jsx
+++ src/context/github/GithubActions.jsx
@@ -5,33 +5,8 @@ const GITHUB_URL = import.meta.env.VITE_APP_GITHUB_URL
 // delete axios.defaults.headers.common["Authorization"]
 const github = axios.create({
   baseURL: GITHUB_URL,
-  // headers: {
-  //   'Content-Type': null
-  // }
-  // headers: { Authorization: ""}
   // headers: { Authorization: `token ${GITHUB_TOKEN}` },
-  // transformRequest: (data, headers) => {
-    // Remove all shared headers
-    // delete headers.common;
-    // or just the auth header
-    // delete headers.common['Authorization'];
-    // delete headers['Authorization'];
-    // }
   })
-  // github.defaults.headers.common = {}
-  // delete github.defaults.headers.common["Authorization"]
-// delete github.defaults.headers.common['Authorization'];
-
-// const awsAxios = axios.create({
-//   transformRequest: (data, headers) => {
-//       // Remove all shared headers
-//       delete headers.common;
-//       // or just the auth header
-//       delete headers.common.Authorization;
-//   }
-// });
-
-
 
 // Get search results
 export const searchUsers = async (text) => {
@@ -51,63 +26,8 @@ export const getUserAndRepos = async (login) => {
     // github.get(`/users/${login}`),               // user request
     // github.get(`/users/${login}/repos`),         // repos request
     github.get(`/users/${login}`),                  // user request
-    github.get(`/users/${login}/repos?${params}`),  // repos request
+    github.get(`/users/${login}/repos?${params}`),  // repos request  (params to get the 10 latest repos)
   ])
 
   return { user: user.data, repos: repos.data }
 }
-
-// // Get search results
-// export const searchUsers = async (text) => {   // text is the typed in text - export to be able to call it from the component (UserSearch.jsx)
-
-//   const params = new URLSearchParams({q: text})   // text is the typed in text
-
-//   // const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-//   //   headers: {
-//   //     Authorization: `token ${GITHUB_TOKEN}`
-//   //   }
-//   // })
-//   // Without using a Token
-//   const response = await fetch(`${GITHUB_URL}/search/users?${params}`)  // params is the typed in text -> the search parameter in the url
-
-//   const { items } = await response.json()   // items are the destructured data wanted
-
-//   return items
-// }
-
-// // Get single user
-// export const getUser = async (login) => {   // login is the username
-
-//   // const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-//   //   headers: {
-//   //     Authorization: `token ${GITHUB_TOKEN}`
-//   //   }
-//   // })
-//   // Without using a Token
-//   const response = await fetch(`${GITHUB_URL}/users/${login}`)  // // params is the typed in text
-
-//   if (response.status === 404) {
-//     window.location = '/notfound'
-//   } else {
-//     const data = await response.json()   // items are the destructured data wanted
-//     return data    
-//   }
-
-// }
-
-// // Get user repos
-// export const getUserRepos = async (login) => {   // login is the username
-
-//   const params = new URLSearchParams({sort: 'created', per_page: 10})   // To just get the 10 latest repos
-
-//   // const response = await fetch(`${GITHUB_URL}/users/${login}/repos`, {     // https://api.github.com/users/bradtraversy/repos
-//   //   headers: {
-//   //     Authorization: `token ${GITHUB_TOKEN}`
-//   //   }
-//   // })
-//   // Without using a Token
-//   const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`)  // params are the search parameters
-
-//   const data = await response.json()   
-//   return data
-// }
